6d78c44bdefb5d2630fef2f9f3425641dae7813c
aefshaeful <aefsaefulrohma042@gmail.com>
2023-10-24 13:19:15 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <h1>Hello, world!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
